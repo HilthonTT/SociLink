5e3801af10690c60bc9b27841055a19c30c9c941
Timothy Tandt <Timothy.tandt@outlook.be>
2023-06-28 19:54:45 +0200

    Implemented UserData
    UserData will do CRU operations.
    
    Modified user
    - added objectIdentifier field.

---
+++ socilink/src/data/userData.ts
@@ -0,0 +1,82 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase/firebase";
+import { User } from "../models/user";
+import { LRUCache } from "lru-cache";
+
+export class UserData {
+  private cacheName = "userData";
+  private collectionName = "users";
+  private userCollectionRef = collection(db, this.collectionName);
+  private cache = new LRUCache({ max: 1000000 });
+
+  public getUsersAsync = async (): Promise<User[]> => {
+    let users = this.cache.get(this.cacheName) as User[];
+
+    if (users === null) {
+      const data = await getDocs(this.userCollectionRef);
+      users = data.docs.map((doc) => ({ ...(doc.data() as User), id: doc.id }));
+      this.cache.set(this.cacheName, users);
+    }
+
+    return users;
+  };
+
+  public getUserAsync = async (id: string): Promise<User> => {
+    const key = `user-${id}`;
+    let user = this.cache.get(key) as User;
+
+    if (user === null) {
+      const userDoc = doc(db, this.collectionName, id);
+      const data = await getDoc(userDoc);
+      user = data.data() as User;
+
+      this.cache.set(key, user);
+    }
+
+    return user;
+  };
+
+  getUserFromAuth = async (objectId: string): Promise<User> => {
+    const key = `user-${objectId}`;
+    let user = this.cache.get(key) as User;
+
+    if (user === null) {
+      const q = query(
+        this.userCollectionRef,
+        where("objectIdentifier", "==", objectId)
+      );
+      const querySnapshot = await getDocs(q);
+      const userDoc = querySnapshot.docs[0];
+
+      if (userDoc) {
+        const userData = userDoc.data() as User;
+        user = {
+          ...userData,
+          id: userDoc.id,
+        };
+
+        this.cache.set(key, user);
+      }
+    }
+
+    return user;
+  };
+
+  public createUserAsync = async (user: User) => {
+    await addDoc(this.userCollectionRef, user);
+  };
+
+  public updateUser = async (user: User) => {
+    const userDoc = doc(db, this.collectionName, user.id);
+    await updateDoc(userDoc, { user });
+  };
+}
--- socilink/src/models/user.ts
+++ socilink/src/models/user.ts
@@ -3,6 +3,7 @@ import { BasicThread } from "./basicThread";
 
 export class User {
   id: string;
+  objectIdentifier: string;
   firstName: string;
   lastName: string;
   displayName: string;
@@ -13,12 +14,14 @@ export class User {
 
   constructor(
     id: string,
+    objectIdentifier: string,
     firstName: string,
     lastName: string,
     displayName: string,
     email: string
   ) {
     this.id = id;
+    this.objectIdentifier = objectIdentifier;
     this.firstName = firstName;
     this.lastName = lastName;
     this.displayName = displayName;
