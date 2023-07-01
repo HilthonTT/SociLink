e6f5bf807a0d9a17fdd2c8dc97035625dbdc45ac
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-01 16:43:07 +0200

    Implemented SampleData page.
    SampleData creates some dummy data to play around with.

--- socilink/src/App.tsx
+++ socilink/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
+import { SampleData } from "./pages/SampleData";
 
 export const App = () => {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/SampleData" element={<SampleData />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
