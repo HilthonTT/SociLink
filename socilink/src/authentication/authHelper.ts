21138fac8fcf117dc9f9a22cfca6cc0ea91a82b4
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-09 16:24:37 +0200

    Improved AuthHelper
    Uses onAuthStateChanged instead which is faster.
    
    authHelper no longer flickers between login page and the current.

--- socilink/src/authentication/authHelper.ts
+++ socilink/src/authentication/authHelper.ts
@@ -1,48 +1,35 @@
 import { useNavigate } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
 import { User } from "../models/user";
 import { IUserData, UserData } from "../data/userData";
-import { useEffect } from "react";
-
-export interface IAuthHelper {
-  getAuthState: () => Promise<User | null>;
-}
-
-export class AuthHelper implements IAuthHelper {
-  private readonly navigate = useNavigate();
-  private readonly userData: IUserData = new UserData();
-
-  public getAuthState = async (): Promise<User | null> => {
-    const [user] = useAuthState(auth);
-
-    if (user === null) {
-      this.navigate("/Login");
-      return null;
-    }
-
-    const userFromAuth = this.userData.getUserFromAuth(user?.uid as string);
-    return userFromAuth;
-  };
-}
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const useAuthHelper = () => {
   const navigate = useNavigate();
   const userData: IUserData = new UserData();
-  const [user] = useAuthState(auth);
+
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     getUserFromAuth();
   }, []);
 
-  const getUserFromAuth = async () => {
-    if (user === null) {
-      navigate("/Login");
-      return null;
-    }
+  const getUserFromAuth = async (): Promise<User | null> => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userFromAuth = await userData.getUserFromAuth(
+          user?.uid as string
+        );
 
-    const userFromAuth = await userData.getUserFromAuth(user?.uid as string);
-    return userFromAuth;
+        setUser(userFromAuth);
+      } else {
+        navigate("/Login");
+        setUser(null);
+      }
+    });
+
+    return user;
   };
 
   return { getUserFromAuth };
