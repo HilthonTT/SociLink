0f614e55a46b3a7ccb39e8b67af339a494f41436
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-18 19:43:33 +0200

    GUI Overhaul Details.tsx
    Changed how details page looks.

--- socilink/src/pages/Details.tsx
+++ socilink/src/pages/Details.tsx
@@ -1,6 +1,12 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { IThreadData, ThreadData } from "../data/threadData";
-import { useEffect, useState } from "react";
+import {
+  Fragment,
+  ReactNode,
+  SyntheticEvent,
+  useEffect,
+  useState,
+} from "react";
 import { Thread } from "../models/thread";
 import { Comment } from "../models/comment";
 import { BasicThread } from "../models/basicThread";
@@ -10,6 +16,59 @@ import { CommentData, ICommentData } from "../data/commentData";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
 import { IUserData, UserData } from "../data/userData";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Checkbox,
+  Container,
+  CssBaseline,
+  Divider,
+  FormControlLabel,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Tab,
+  Tabs,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { AccountBox, ModeEdit, Send } from "@mui/icons-material";
+
+interface TabPanelProps {
+  children?: ReactNode;
+  index: number;
+  value: number;
+}
+
+const CustomTabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}>
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+function a11yProps(index: number) {
+  return {
+    id: `thread-tab-${index}`,
+    "aria-controls": `thread-tabpanel-${index}`,
+  };
+}
 
 export const Details = () => {
   const { id } = useParams();
@@ -19,11 +78,13 @@ export const Details = () => {
   const threadData: IThreadData = new ThreadData();
   const commentData: ICommentData = new CommentData();
   const userData: IUserData = new UserData();
+  const imageWidthHeight = 200;
 
   const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
   const [thread, setThread] = useState<Thread | null>(null);
   const [comments, setComments] = useState<Comment[] | null>(null);
   const [comment, setComment] = useState("");
+  const [value, setValue] = useState(0);
 
   const getThreadAsync = async () => {
     const fetchedThread = await threadData.getThreadAsync(id as string);
@@ -33,6 +94,10 @@ export const Details = () => {
     }
 
     setThread(fetchedThread);
+  };
+
+  const handleValueChange = (event: SyntheticEvent, newValue: number) => {
+    setValue(newValue);
   };
 
   const getThreadCommentsAsync = async () => {
@@ -75,49 +140,130 @@ export const Details = () => {
 
   useEffect(() => {
     getThreadAsync();
-  }, []);
+  }, [id]);
 
   useEffect(() => {
     getThreadCommentsAsync();
-  }, []);
+  }, [comments]);
 
   return (
-    <div>
-      {thread ? (
-        <div>
-          {comments?.length}
-          <div>{thread?.thread}</div>
-          <div>{thread?.description}</div>
-          <br />
-          <br />
-          <div>
-            <label htmlFor="comment">Comment here</label>
-            <textarea
-              id="comment"
-              placeholder="Comment"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
+    <Container>
+      <CssBaseline />
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: "divider",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        {thread?.downloadUrl ? (
+          <Avatar
+            alt={thread.thread}
+            src={thread.downloadUrl}
+            sx={{ width: imageWidthHeight, height: imageWidthHeight }}
+          />
+        ) : (
+          <Avatar
+            sx={{
+              m: 1,
+              bgcolor: "primary.main",
+              width: imageWidthHeight,
+              height: imageWidthHeight,
+            }}>
+            <AccountBox
+              sx={{ width: imageWidthHeight / 2, height: imageWidthHeight / 2 }}
             />
-            <div>
-              <button type="button" onClick={onCommentAsync}>
-                Comment
-              </button>
-            </div>
-          </div>
-          <div>
-            <label>Comments</label>
-            <ul>
-              {comments?.map((c) => (
-                <li key={c.id}>
-                  {c.comment} <strong>By {c.author.displayName}</strong>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      ) : (
-        <div>No Thread Found.</div>
-      )}
-    </div>
+          </Avatar>
+        )}
+        <Tabs
+          value={value}
+          onChange={handleValueChange}
+          aria-label="thread-tabs">
+          <Tab label="Thread Information" {...a11yProps(0)} />
+          <Tab label="Comments" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Typography
+          component="h2"
+          variant="h5"
+          className="text-underline"
+          sx={{ textTransform: "uppercase", fontWeight: "bold" }}>
+          Thread Information
+        </Typography>
+        <Box sx={{ justifyContent: "space-between", display: "flex" }}>
+          <Typography component="div" variant="h6">
+            Thread name: {thread?.thread}
+          </Typography>
+          {thread?.author.id === loggedInUser?.id && (
+            <Button startIcon={<ModeEdit />} />
+          )}
+        </Box>
+        <Box sx={{ justifyContent: "space-between", display: "flex" }}>
+          <Typography component="div" variant="h6">
+            Author: {thread?.author.displayName}
+          </Typography>
+        </Box>
+        <Box sx={{ justifyContent: "space-between", display: "flex" }}>
+          <Typography component="div" variant="h6">
+            Date Posted: {thread?.dateCreated.toDate().toLocaleDateString()}
+          </Typography>
+        </Box>
+        <Divider sx={{ borderWidth: 2 }} />
+        <Box sx={{ justifyContent: "space-between", display: "flex" }}>
+          <Typography
+            component="div"
+            variant="h6"
+            sx={{ wordWrap: "break-word" }}>
+            {thread?.description}
+          </Typography>
+          {thread?.author.id === loggedInUser?.id && (
+            <Button startIcon={<ModeEdit />} />
+          )}
+        </Box>
+        <Typography></Typography>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Box component="div">
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Comment"
+            type="text"
+            id="comment"
+            onChange={(e) => setComment(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={onCommentAsync} edge="end">
+                    <Send />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Box component="div">
+          {comments?.map((c) => (
+            <Card key={c.id} sx={{ minWidth: 275, marginBottom: 2 }}>
+              <CardContent>
+                <Typography variant="body1" component="div">
+                  {c.comment}
+                  <br />
+                  By: {c.author.displayName}
+                </Typography>
+              </CardContent>
+              {c?.author.id === loggedInUser?.id && (
+                <CardActions>
+                  <Button size="small">Edit</Button>
+                </CardActions>
+              )}
+            </Card>
+          ))}
+        </Box>
+      </CustomTabPanel>
+    </Container>
   );
 };
