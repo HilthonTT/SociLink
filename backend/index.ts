29c734f469e82cae5eed9f517da9b869c5b7d035
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-20 20:12:33 +0200

    Implemented Express API
    The Base is there.

---
+++ backend/index.ts
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+
+import mongoose, { ConnectOptions } from "mongoose";
+const appSettings: AppSettings = require("./config/appsettings_dev.json");
+
+interface AppSettings {
+  mongoDb: {
+    ConnectionString: string;
+    DbName: string;
+  };
+}
+
+const settings = appSettings as AppSettings;
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURI = settings.mongoDb.ConnectionString;
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+connectToMongoDB();
+
+app.listen(port, () => {
+  console.log("Server running on port 3001");
+});
